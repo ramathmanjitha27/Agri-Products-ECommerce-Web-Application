6115d6e8741799f923570f616556ac2788cb42e3
Ramath <ramathmanjitha@gmail.com>
2022-05-18 08:53:27 +0530

    pull commit

--- Buyer/server.js
+++ Buyer/server.js
@@ -1,26 +1,32 @@
-const express = require('express');
-const mongoose = require('mongoose');
-// const bodyParser = require('body-parser');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
 const app = express();
-const cors = require('cors');
-// const passport = require('passport');
+require("dotenv").config();
 
 require('dotenv').config();
+
+const URL = process.env.MONGODB_URL;
 const PORT = process.env.PORT || 8000;
 app.use(cors());
-// app.use(bodyParser.json());
-const URL = process.env.MONGO_URI;
-mongoose.connect(URL, {
+app.use(bodyParser.json());
+
+
+ongoose.connect(URL, {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
-});
+})
+
 const connection = mongoose.connection;
-connection.once('open', () => {
-    console.log('Mongodb Connection Success!');
-});
+connection.once("open", () => {
+    console.log("Mongodb connection success");
+})
 
 app.listen(PORT,()=>{
     console.log(`App is running on ${PORT}`);
 });
+
