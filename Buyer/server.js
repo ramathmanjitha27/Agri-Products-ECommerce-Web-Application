742c44303c693fd0707eec6a6790aa33d089177c
Ramath <ramathmanjitha@gmail.com>
2022-05-18 07:39:48 +0530

    add Item

--- Buyer/server.js
+++ Buyer/server.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const mongoose = require('mongoose');
 // const bodyParser = require('body-parser');
@@ -6,23 +7,16 @@ const cors = require('cors');
 // const passport = require('passport');
 
 require('dotenv').config();
-const PORT = process.env.PORT || 7000;
+const PORT = process.env.PORT || 8000;
 app.use(cors());
 // app.use(bodyParser.json());
-const URL = process.env.MONGO_URI;
+const URL = process.env.MONGODB_URI;
 mongoose.connect(URL);
 const connection = mongoose.connection;
 connection.once('open', () => {
     console.log('Mongodb Connection Success!');
 });
 
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
-
-app.use('/api/farmer', require('../Farmer/routes/itemRoutes'))
-
 app.listen(PORT,()=>{
     console.log(`App is running on ${PORT}`);
 });
-//routes
-
