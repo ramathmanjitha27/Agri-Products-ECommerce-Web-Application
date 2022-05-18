8979f410fbc625fec5a09a916e4d12600bf86e95
Suboda Dabarera <88880898+SubodaDabarera@users.noreply.github.com>
2022-05-18 12:16:55 +0530

    payment service added with credit card

--- PaymentService/server.js
+++ PaymentService/server.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const app = express();
 const cors = require('cors');
 const dotenv = require('dotenv');
-// const passport = require('passport');
 
 require('dotenv').config();
 
 const PORT = process.env.PORT || 9000;
 app.use(cors());
-// app.use(bodyParser.json());
+app.use(bodyParser.json());
 
 const URL = process.env.MONGODB_URL;
 
@@ -21,15 +20,9 @@ connection.once('open', () => {
     console.log('Mongodb Connection Success!');
 });
 
+app.use('/api/creditCard', require('./routes/paymentRoutes'))
+
+
 app.listen(PORT,()=>{
     console.log(`App is running on ${PORT}`);
 });
-
-//routes
-// const PostRoutes = require('./routes/posts');
-//
-// app.use('/post',PostRoutes);
-
-
-
-
