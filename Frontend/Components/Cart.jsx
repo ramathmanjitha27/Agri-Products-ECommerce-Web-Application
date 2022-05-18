e4294675e14e49924a192bde1ee06101337f035c
it20114312 <it20114312@my.sliit.lk>
2022-05-18 18:38:42 +0530

    buyer frontend

---
+++ Frontend/Components/Cart.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function cart(){
+    return(
+        <div className="container">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Name of product</th>
+                        <th>Price</th>
+                        <th>Quantity</th>
+                        <th>Delete Item</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
+}
