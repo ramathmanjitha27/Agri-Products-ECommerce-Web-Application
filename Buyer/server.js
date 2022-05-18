2fc24590bd66b1f5226f7a1feab9347bdcf27e0f
Ramath <ramathmanjitha@gmail.com>
2022-05-18 07:47:25 +0530

    add Item

--- Buyer/server.js
+++ Buyer/server.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const mongoose = require('mongoose');
 // const bodyParser = require('body-parser');
@@ -10,9 +9,14 @@ require('dotenv').config();
 const PORT = process.env.PORT || 8000;
 app.use(cors());
 // app.use(bodyParser.json());
-const URL = process.env.MONGODB_URI;
-mongoose.connect(URL);
-const connection = mongoose.connection;
+const URL = process.env.MONGO_URI;
+mongoose.connect(URL, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+});
+const connection = mongoose.connection();
 connection.once('open', () => {
     console.log('Mongodb Connection Success!');
 });
