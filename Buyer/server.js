eba4cd3e0ebb94d904eb1decf742fd180f302864
it20114312 <it20114312@my.sliit.lk>
2022-05-19 11:36:39 +0530

    frontend cart view

--- Buyer/server.js
+++ Buyer/server.js
@@ -11,6 +11,7 @@ require('dotenv').config();
 const PORT = process.env.PORT || 8000;
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({extended:false}))
 // app.use(bodyParser.json());
 
 const URL = process.env.MONGODB_URL;
