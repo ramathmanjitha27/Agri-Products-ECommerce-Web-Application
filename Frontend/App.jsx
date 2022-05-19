39fea0985255dcac31538551b8b128a560548860
Ramath <ramathmanjitha@gmail.com>
2022-05-19 12:32:05 +0530

    view Item frontend

--- Frontend/App.jsx
+++ Frontend/App.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import NavBar from "./Components/NavBar";
 import Cart from './Components/Cart'
 import {BrowserRouter,Route,Routes} from "react-router-dom";
+import AddItemFarmer from "./Components/AddItemFarmer";
+import Farmer from "./Components/Farmer";
+import ViewItemFarmer from "./Components/ViewItemFarmer";
 
 function App(){
     return(
@@ -10,6 +13,9 @@ function App(){
             <BrowserRouter>
                 <Routes>
                     <Route path="/Cart" element={<Cart/>}/>
+                    <Route path="/FarmerHome" element ={<Farmer/>}/>
+                    <Route path="/AddItem" element ={<AddItemFarmer/>}/>
+                    <Route path="/viewitem/:id" element={<ViewItemFarmer/>}/>
                 </Routes>
             </BrowserRouter>
 
