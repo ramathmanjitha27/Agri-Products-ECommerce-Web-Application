0faa638d2a51761e79eb8c8087cecea075fa061e
Suboda Dabarera <88880898+SubodaDabarera@users.noreply.github.com>
2022-05-19 14:51:16 +0530

    backend updated

--- PaymentService/models/creditCardModel.js
+++ PaymentService/models/creditCardModel.js
@@ -18,6 +18,10 @@ const creditCardSchema = new Schema({
             type: Number,
             required: true
         },
+        holderName:{
+            type:String,
+            required: true
+        }
     },
     {
         timestamps: true
