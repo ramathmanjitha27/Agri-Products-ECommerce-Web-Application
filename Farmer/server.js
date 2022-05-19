64a32cf090e071d50c6e89e7658fa5ff34da4673
Ramath <ramathmanjitha@gmail.com>
2022-05-19 12:36:28 +0530

    conflict resolve

--- Farmer/server.js
+++ Farmer/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+//const bodyParser = require('body-parser');
 const app = express();
 const cors = require('cors');
 const dotenv = require('dotenv');
@@ -11,7 +11,7 @@ require('dotenv').config();
 const PORT = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
 
 const URL = process.env.MONGODB_URL;
 
